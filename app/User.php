0dc5126f268adc09eb8a05d300bd5cf8808adb0a
alireza <mefroen@gmail.com>
2021-10-01 23:38:45 +0330

    make a relation in user model

--- app/User.php
+++ app/User.php
@@ -15,4 +15,10 @@ class User extends Model
     protected $fillable = ['email', 'first_name', 'last_name', 'avatar', 'status', 'is_active', 'password', 'verify_token', 'user_type', 'remember_token', 'remember_token_expire'];
     protected $deletedAt = 'deleted_at';
 
+    
+    public function category(){
+        return $this->hasMany('\App\User', 'user_id', 'id');
+    }
+
+
 }
