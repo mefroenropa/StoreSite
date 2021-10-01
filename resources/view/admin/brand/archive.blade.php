77fd8b48341a38d9f197496481c865843c94bfe9
alireza <mefroen@gmail.com>
2021-10-01 19:35:27 +0330

    create and layering brand views

---
+++ resources/view/admin/brand/archive.blade.php
@@ -0,0 +1,74 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+    <title>برند هایه بایگانی شده</title>
+    <link rel="stylesheet" type="text/css" href="<?= asset('src/plugins/jquery-steps/jquery.steps.css')?>">
+
+@endsection
+
+@section('content')
+<div class="main-container">
+    <div class="pd-ltr-20">
+
+        <div class="pd-20 card-box mb-30">
+            <div class="clearfix">
+                <div class="pull-right">
+                    <h4 class="text-blue h4 text-right">برند ها</h4>
+                    <p class="mb-30 text-right">لیست همه برند هایه بایگانی شده  </p>
+                </div>
+                <div class="pull-left">
+                    <a href="index" class="btn btn-danger d-inline">بازگشت</a>
+                </div>
+            </div>
+ 
+            <div class="card-box mb-30 " style="text-align: right;">
+				<table class="data-table table nowrap">
+					<thead>
+						<tr>
+							<th># </th>
+
+							<th class="table-plus datatable-nosort">نام</th>
+							<th>سازنده </th>
+							<th>تعداد محصولات زیر دسته</th>
+							
+							<th class="datatable-nosort">عملیات</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr >
+							<td>1</td>
+
+							<td> سامسونگ </td>
+							<td> علیرضا جوادی </td>
+
+							<td> <h6 class="alert alert-danger">29</h6> </td>
+							
+							
+							<td>
+								<div class="dropdown">
+									<a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+										<i class="dw dw-more"></i>
+									</a>
+									<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+										<a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> ویرایش</a>
+										<a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> حذف</a>
+									</div>
+								</div>
+							</td>
+						</tr>
+
+
+
+						
+					</tbody>
+				</table>
+			</div>
+        </div>
+  
+
+@endsection
+
+
+            
+
+							
---
+++ resources/view/admin/brand/create.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.master')
+
+@section('head-tag')
+    <title>افزودن برند جدید</title>
+    <link rel="stylesheet" type="text/css" href="<?= asset('src/plugins/jquery-steps/jquery.steps.css')?>">
+
+@endsection
+
+@section('content')
+<div class="main-container">
+    <div class="pd-ltr-20">
+
+        <div class="pd-20 card-box mb-30">
+            <div class="clearfix">
+                <div class="clearfix">
+                    <div class="pull-right">
+                        <h4 class="text-blue h4 text-right"> افزودن برند </h4>
+                    </div>
+                    <div class="pull-left">
+                        <a href="index" class="btn btn-danger d-inline">بازگشت</a>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="pd-20 card-box mb-30 text-right">
+            	<form>
+                        <div class="form-group">
+                            <label>نام برند</label>
+                            <input class="form-control" name="na222me" type="text">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label> عکس برند مورد نظر را انتخاب کنید </label>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input">
+                                <label class="custom-file-label">Choose file</label>
+                            </div>
+                        </div>
+                    
+                </form>
+            
+            </div>
+
+        </div>
+
+
+@endsection
