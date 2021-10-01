44a2cf1f8aabeaf5c36311514f3d0b655649032f
alireza <mefroen@gmail.com>
2021-10-01 23:34:50 +0330

    AdsRequest change name to ProductRequest

---
+++ app/Http/Requests/Admin/ProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use System\Request\Request;
+
+class ProductRequest extends Request
+{
+    public function rules(){
+ if(methodField() == 'put'){
+    return [
+        'title' => "required|max:191",
+        'body' => "required|max:1000",
+        'attributes' => "required",
+        'amount' => "required",
+        'discount' => "required",
+        'brand_id' => "required",
+        'user_id' => "required",
+        'cat_id' => "required",
+       
+      
+    ];
+ }else{
+    return [
+        'title' => "required|max:191",
+        'body' => "required|max:1000",
+        'attributes' => "required",
+        'amount' => "required",
+        'discount' => "required",
+        'brand_id' => "required",
+        'user_id' => "required",
+        'cat_id' => "required",
+    ];
+ }
+    }
+}
