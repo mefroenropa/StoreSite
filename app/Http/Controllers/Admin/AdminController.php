75e429d8deebf1dec13b9e80ea1b62f9771235dd
alireza <mefroen@gmail.com>
2021-10-01 23:32:09 +0330

    fix problem loginById

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,6 +10,7 @@ class AdminController extends Controller
 
     public function __construct(){
 
+        Auth::loginById(1);
         Auth::check();
         if(Auth::user()->user_type != 'admin'){
             redirect('/login');
