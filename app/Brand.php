0480a460192ebe36a1f4d3bf40988e14ef041816
alireza <mefroen@gmail.com>
2021-10-02 20:12:50 +0330

    fix a problem in brand model

--- app/Brand.php
+++ app/Brand.php
@@ -12,9 +12,18 @@ class Brand extends Model
     use HasSoftDelete;
 
     protected $table = "brands";
-    protected $fillable = ['name', 'user_id', 'parent_id'];
+    protected $fillable = ['name', 'user_id', 'image'];
+    protected $casts = ['image' => 'array'];
+
     protected $deletedAt = 'deleted_at';
 
+    public function user(){
+        return $this->belongsTo('\App\User', 'user_id', 'id');
+    }
+
+    public function products(){
+        return $this->hasMany("\App\Product", 'brand_id', 'id');
+    }
 
 
 }
