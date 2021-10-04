9b6acecada9024b58c62d3d9eb7abe830be7fb3d
alireza <mefroen@gmail.com>
2021-10-04 22:55:34 +0330

    add image to categories

--- app/Http/Requests/Admin/CategoryRequest.php
+++ app/Http/Requests/Admin/CategoryRequest.php
@@ -6,10 +6,22 @@ use System\Request\Request;
 
 class CategoryRequest extends Request
 {
-    public function rules(){
-        return [
-            'name' => "required|max:191",
-            'parent_id' => "exists:categories,id"
-        ];
+    public function rules()
+    {
+        if (methodField() == 'put') {
+            return [
+                'name' => "required|max:191",
+                'englishName' => "required|max:191",
+                'image' => 'file|mimes:jpeg,jpg,png,gif',
+                'parent_id' => "exists:categories,id"
+            ];
+        } else {
+            return [
+                'name' => "required|max:191",
+                'englishName' => "required|max:191",
+                'image' => 'required|file|mimes:jpeg,jpg,png,gif',
+                'parent_id' => "exists:categories,id"
+            ];
+        }
     }
 }
