1ab5ecab65e9d9fd6a85e590319af0495916b730
alireza <mefroen@gmail.com>
2021-10-05 12:10:55 +0330

    create constructor for check login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use System\Auth\Auth;
+use System\Session\Session;
 
 class Controller{
     public function __construct()
     {
-        // Auth::loginById(1);   
+        if(empty(Session::get('user'))){
+
+            Auth::loginById(1);   
+        }
     }
 }
