5d4608615e931adbbcca0a16d1e2cbeef3a0a2cd
alireza <mefroen@gmail.com>
2021-10-06 12:34:12 +0330

    update cart model and database table new way to checkout product

--- app/Cart.php
+++ app/Cart.php
@@ -12,7 +12,7 @@ class Cart extends Model
     use HasSoftDelete;
 
     protected $table = "cart";
-    protected $fillable = ['user_id', 'count', 'isPaid', 'product_id'];
+    protected $fillable = ['user_id', 'count', 'isPaid', 'product_id', 'price'];
     protected $deletedAt = 'deleted_at';
 
     public function user(){
@@ -21,6 +21,41 @@ class Cart extends Model
 
     public function product(){
         return $this->belongsTo('\App\Product', 'product_id', 'id');
+    }
+
+    public function isDone(){
+        switch($this->isPaid){
+            case 0:
+                return '<h4 class="alert alert-warning">پرداخت نشده </h4>';
+            break; 
+
+            case 1:
+                return '<h4 class="alert alert-success"> پرداخت شده </h4>';
+            break; 
+
+            
+        }
+    }
+
+ 
+    public function status(){
+        switch($this->status){
+            case 0:
+                return '<h4 class="alert alert-warning"> در حال پردازش </h4>';
+            break; 
+
+            case 1:
+                return '<h4 class="alert alert-primary"> در حال ارسال </h4>';
+            break; 
+
+            case 2:
+                return '<h4 class="alert alert-info"> تحویل داده شده </h4>';
+            break; 
+
+            case 3:
+                return '<h4 class="alert alert-danger"> مرجوعی </h4>';
+            break;
+        }
     }
 
 }
